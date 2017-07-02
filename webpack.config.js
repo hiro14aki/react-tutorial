b785cb66df7efc30b1ca81a18fa49ad30a18c27f
Hiroaki Imai <hiro.1114.aki@gmail.com>
2017-07-03 02:06:03 +0900

    Reactのビルド環境を整備(yarn, package.json, webpack)

---
+++ webpack.config.js
@@ -0,0 +1,119 @@
+// let webpack = require('webpack');
+
+const path = require('path')
+// const ExtractTextPlugin = require("extract-text-webpack-plugin")
+//
+// const extractSass = new ExtractTextPlugin({
+//   filename: "./css/style.min.css",
+//   disable: process.env.NODE_ENV === "development"
+// })
+//
+// const extractTodoSass = new ExtractTextPlugin({
+//   filename: "./css/todo.min.css",
+//   disable: process.env.NODE_ENV === "development"
+// })
+
+module.exports = {
+  entry: {
+    app: "./src/app.js"
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './js/[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
+
+// module.exports = [{
+//   entry: './src/app.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: './js/app.bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: 'babel-loader'
+//       },
+//       {
+//         test: /\.vue$/,
+//         loader: 'vue-loader'
+//       },
+//       {
+//         test: /\.css$/,
+//         loader: "style-loader!css-loader",
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: extractSass.extract({
+//           use: [{
+//             loader: "css-loader"
+//           }, {
+//             loader: "sass-loader"
+//           }],
+//           // use style-loader in development
+//           fallback: "style-loader"
+//         })
+//       }
+//     ]
+//   },
+//   plugins: [
+//     extractSass
+//   ],
+//   devtool: 'source-map'
+// },{
+//   entry: './src/todo.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: './js/todo.app.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.js$/,
+//         exclude: /node_modules/,
+//         use: 'babel-loader'
+//       },
+//       {
+//         test: /\.vue$/,
+//         loader: 'vue-loader'
+//       },
+//       {
+//         test: /\.css$/,
+//         loader: "style-loader!css-loader",
+//       },
+//       {
+//         test: /\.scss$/,
+//         use: extractTodoSass.extract({
+//           use: [{
+//             loader: "css-loader"
+//           }, {
+//             loader: "sass-loader"
+//           }],
+//           // use style-loader in development
+//           fallback: "style-loader"
+//         })
+//       }
+//     ]
+//   },
+//   plugins: [
+//     extractTodoSass
+//   ],
+//   resolve: {
+//     alias: {
+//       'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+//     }
+//   },
+//   devtool: 'source-map'
+// }];
